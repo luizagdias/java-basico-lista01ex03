e5f106fe7a62779f3e46d8ffc1507f971609c270
Luiza <luizagdias9@gmail.com>
2022-02-22 21:21:43 -0300

    feat: java-exercicio-03

---
+++ Main.java
@@ -0,0 +1,27 @@
+package br.fai.ex03;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.start();
+    }
+
+    private void start() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite uma idade: ");
+        int idade = scanner.nextInt();
+
+        if (idade > 20) {
+            System.out.println("A idade é maior que 20");
+        } else if (idade > 12 && idade < 17) {
+            System.out.println("A idade está entre 12 e 17");
+        } else if (idade == 7 || idade < 5) {
+            System.out.println("A idade é igual a 7 ou menor do que 5");
+        } else {
+            System.out.println("Outras combinações");
+        }
+    }
+}
